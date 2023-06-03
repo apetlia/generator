57e8769c0fea5500eed0d7aedd5615559b732c29
Artem Petlia <pak@servers.com>
2023-06-03 10:27:02 +0300

    add cope btn and notify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import nunjucks from 'nunjucks';
 import yaml from 'js-yaml';
 
-const form = document.querySelector('.form');
-const result = document.querySelector('.result');
+const form = document.querySelector('.js-form');
+const outputResult = document.querySelector('.js-output-result');
+const outputParams = document.querySelector('.js-output-params');
+const copyBtn = document.querySelector('.js-btn-copy');
 
 form.addEventListener('submit', generateConfig);
+copyBtn.addEventListener('click', copy);
 
 nunjucks.configure({
   autoescape: true,
@@ -19,5 +22,18 @@ function generateConfig(evt) {
 
   const params = yaml.load(paramsString);
 
-  result.innerHTML = nunjucks.renderString(templateString, params);
+  outputParams.innerHTML = JSON.stringify(params, null, 4);
+
+  outputResult.innerHTML = nunjucks.renderString(templateString, params);
+}
+
+function copy() {
+  navigator.clipboard.writeText(outputResult.textContent).then(notifyCopy);
+}
+
+function notifyCopy() {
+  copyBtn.textContent = 'Copied';
+  setTimeout(() => {
+    copyBtn.textContent = 'Copy output';
+  }, 1000);
 }
